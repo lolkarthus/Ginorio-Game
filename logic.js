04fb3254b0072d683dde825f8f4d76408e672987
lolkarthus <basu.lolkarthus@gmail.com>
2019-07-31 12:21:35 -0700

    Started new branch
    Implemented animation, font, and a temp sound effect

--- logic.js
+++ logic.js
@@ -1,3 +1,9 @@
-document.getElementById('start').onclick = function() {
-   alert("button was clicked");
+var snd = new Audio("button.mp3");
+
+document.getElementById('start').onclick = function replace() {
+	snd.play()
+	snd.currentTime=0;
+	$('#start').fadeOut(1000)
+	$('#intro').fadeOut(1000)
+	
 }
