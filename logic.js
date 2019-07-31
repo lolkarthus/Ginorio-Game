244ce6a9f35d72256fa174376b6d4b108067a1c6
lolkarthus <basu.lolkarthus@gmail.com>
2019-07-31 14:13:54 -0700

    Added Race Selection
    Races and stats are now implemented. Need photos of Elf ginorio, human ginorio and dwarf ginorio

--- logic.js
+++ logic.js
@@ -1,9 +1,54 @@
-var snd = new Audio("button.mp3");
+var snd = new Audio("button.mp3")
+var int = 0
+var cha = 0
+var str = 0
+var dex = 0
+var vit = 0
+
+
+function select(race) {
+	if (race == 'Human') {
+		console.log('U R a human')
+		int = 1
+		cha = 1
+		str = 0
+		dex = 0
+ 		vit = 0
+	}
+	if (race == 'Elf') {
+			console.log('U R an elf')
+		dex = 1
+		int = 1
+		str = 0
+		cha = 0
+ 		vit = 0
+		}
+	if (race == 'Dwarf') {
+		console.log('U R a dwarf')
+		str = 1
+		vit = 1
+		int = 0
+		dex = 0
+ 		cha = 0
+	}
+	console.log('int is ' + int)
+	console.log('cha is ' + cha)
+	console.log('str is ' + str)
+	console.log('dex is ' + dex)
+	console.log('vit is ' + vit)
+}
+
 
 document.getElementById('start').onclick = function replace() {
 	snd.play()
 	snd.currentTime=0;
 	$('#start').fadeOut(1000)
 	$('#intro').fadeOut(1000)
-	
+	$('#raceinfo').delay(1000).fadeIn(500)
+	$('.table').delay(1000).fadeIn(500)
 }
+
+
+
+
+
